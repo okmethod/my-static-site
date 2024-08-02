a9fc1895a801ad94eced983b4922a68b14a9a2c1
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-03 02:45:20 +0900

    :sparkle: init pages

--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import "../app.postcss";
   import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+  import { Toast, Modal, initializeStores } from "@skeletonlabs/skeleton";
+  import Icon from "@iconify/svelte";
+
+  initializeStores();
 
   // Highlight JS
   import hljs from "highlight.js/lib/core";
@@ -23,42 +27,26 @@
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<!-- App Shell -->
-<AppShell>
-  <svelte:fragment slot="header">
-    <!-- App Bar -->
-    <AppBar>
-      <svelte:fragment slot="lead">
-        <strong class="text-xl uppercase">Skeleton</strong>
-      </svelte:fragment>
-      <svelte:fragment slot="trail">
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://discord.gg/EXqV7W8MtY"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Discord
+<svelte:head>
+  <title>OK SITE</title>
+</svelte:head>
+
+<Modal />
+<Toast position="tr" rounded="rounded-lg" />
+
+<div class="flex min-h-screen flex-col">
+  <div class="border-b border-gray-400">
+    <AppBar class="!p-2">
+      <div class="flex items-center">
+        <a href="/" class="flex items-center">
+          <Icon icon="mdi-light:home" height="20" />
+          <div class="text-lg">Top</div>
         </a>
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://twitter.com/SkeletonUI"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Twitter
-        </a>
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://github.com/skeletonlabs/skeleton"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </svelte:fragment>
+      </div>
     </AppBar>
-  </svelte:fragment>
-  <!-- Page Route Content -->
-  <slot />
-</AppShell>
+  </div>
+
+  <div class="container mx-auto flex-1 overflow-auto mt-4">
+    <slot />
+  </div>
+</div>
