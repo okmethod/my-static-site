87cb563f7fd9c957744a177560add65eca46c251
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-03 02:27:46 +0900

    :shirt: formatter

--- skeleton-app/svelte.config.js
+++ skeleton-app/svelte.config.js
@@ -1,32 +1,32 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
-	
-	vitePlugin: {
-		inspector: true,
-	},
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		appDir: 'app',
-		adapter: adapter({
-			pages: "build",
-			assets: "build",
-			fallback: "index.html",
-			precompress: false,
-			strict: true,
-		}),
-		paths: {
-			// Github Pagesで公開する場合は、base にリポジトリ名を指定
-			base: '/my-static-site'
-		},
-	}
+  extensions: [".svelte"],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess()],
+
+  vitePlugin: {
+    inspector: true,
+  },
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    appDir: "app",
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
+      precompress: false,
+      strict: true,
+    }),
+    paths: {
+      // Github Pagesで公開する場合は、base にリポジトリ名を指定
+      base: "/my-static-site",
+    },
+  },
 };
 export default config;
--- skeleton-app/vite.config.ts
+++ skeleton-app/vite.config.ts
@@ -1,9 +1,9 @@
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { purgeCss } from "vite-plugin-tailwind-purgecss";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()],
-	// Github Pagesで公開する場合は、base にリポジトリ名を指定
-	base: '/my-static-site/'
+  plugins: [sveltekit(), purgeCss()],
+  // Github Pagesで公開する場合は、base にリポジトリ名を指定
+  base: "/my-static-site/",
 });
