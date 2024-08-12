68ada49171e419b8bf93beabbfe959af35f391a7
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-12 18:55:37 +0900

    :+1: adjust root page and navigate example

--- skeleton-app/src/routes/(root)/+page.svelte
+++ skeleton-app/src/routes/(root)/+page.svelte
@@ -1,5 +1,24 @@
-<div class="container h-full mx-auto flex justify-center items-center">
-  <div class="space-y-5">
-    <h1 class="h1">Hello, Welcome to My Static Site !</h1>
+<script lang="ts">
+  import { navigateTo } from "$lib/utils/navigation.client";
+  import { GITHUB_REPO_URL } from "$lib/constants/common";
+</script>
+
+<div class="cRouteBodyStyle">
+  <!-- タイトル部 -->
+  <div class="cTitlePartStyle md:!mb-4">
+    <h1 class="cTitleStyle md:!text-3xl">Hello, Welcome to My Static Site !</h1>
+  </div>
+
+  <!-- コンテンツ部 -->
+  <div class="cContentPartStyle !ml-4">
+    <button on:click|preventDefault={() => navigateTo("/example")} class="cLinkButtonStyle md:!text-2xl">
+      次のページ
+    </button>
+    <button
+      on:click|preventDefault={() => window.open(GITHUB_REPO_URL, "_blank")}
+      class="cLinkButtonStyle md:!text-2xl"
+    >
+      リポジトリ
+    </button>
   </div>
 </div>
