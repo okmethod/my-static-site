68be15aa1c5158c61e00496c69448f926ba4cb5a
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-12 18:43:42 +0900

    :+1: adjust main layout

--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -2,8 +2,6 @@
   import "../app.postcss";
   import { AppBar } from "@skeletonlabs/skeleton";
   import { Toast, Modal, initializeStores } from "@skeletonlabs/skeleton";
-  import Icon from "@iconify/svelte";
-
   initializeStores();
 
   // Highlight JS
@@ -14,7 +12,6 @@
   import css from "highlight.js/lib/languages/css";
   import javascript from "highlight.js/lib/languages/javascript";
   import typescript from "highlight.js/lib/languages/typescript";
-
   hljs.registerLanguage("xml", xml); // for HTML
   hljs.registerLanguage("css", css);
   hljs.registerLanguage("javascript", javascript);
@@ -25,28 +22,29 @@
   import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
   import { storePopup } from "@skeletonlabs/skeleton";
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+  import { navigateTo } from "$lib/utils/navigation.client";
 </script>
 
 <svelte:head>
-  <title>OK SITE</title>
+  <title>PokeAPI Stadium</title>
 </svelte:head>
 
 <Modal />
 <Toast position="tr" rounded="rounded-lg" />
 
-<div class="flex min-h-screen flex-col">
-  <div class="border-b border-gray-400">
-    <AppBar class="!p-2">
-      <div class="flex items-center">
-        <a href="/" class="flex items-center">
-          <Icon icon="mdi-light:home" height="20" />
-          <div class="text-lg">Top</div>
+<div class="flex flex-col h-screen">
+  <div class="mb-4 border-b border-gray-400">
+    <AppBar class="!p-1 md:!p-2">
+      <div class="flex items-center h-full">
+        <a href="/" class="flex items-center" on:click|preventDefault={() => navigateTo("/")}>
+          <div class="text-sm md:text-lg">TOP</div>
         </a>
       </div>
     </AppBar>
   </div>
 
-  <div class="container mx-auto flex-1 overflow-auto mt-4">
+  <div class="container mx-auto overflow-y-auto pb-16">
     <slot />
   </div>
 </div>
