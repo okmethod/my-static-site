3e70213636c7c4178368f39a91151bef7efc23a5
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-09 08:45:09 +0900

    :+1: applyTheme from themeStore

--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -9,7 +9,22 @@
   import { storePopup } from "@skeletonlabs/skeleton";
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
+  import { onMount } from "svelte";
+  import { applyTheme, setTheme } from "$lib/stores/theme";
   import { navigateTo } from "$lib/utils/navigation.client";
+
+  let isLoaded = false;
+  onMount(async () => {
+    function wait(ms: number) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    await Promise.all([applyTheme(), wait(500)]);
+    isLoaded = true;
+  });
+
+  function toggleTheme() {
+    setTheme({ name: "sahara", dark: false });
+  }
 </script>
 
 <svelte:head>
@@ -19,18 +34,28 @@
 <Modal />
 <Toast position="tr" rounded="rounded-lg" />
 
-<div class="flex flex-col h-screen">
-  <div class="border-b border-gray-400">
-    <AppBar class="!p-1 md:!p-2">
-      <div class="flex items-center h-full">
-        <a href="/" class="flex items-center" on:click|preventDefault={() => navigateTo("/")}>
-          <div class="text-sm md:text-lg">TOP</div>
-        </a>
-      </div>
-    </AppBar>
-  </div>
+{#if isLoaded}
+  <div class="flex flex-col h-screen">
+    <div class="border-b border-gray-400">
+      <AppBar class="!p-1 md:!p-2">
+        <div class="flex items-center h-full">
+          <a href="/" class="flex items-center" on:click|preventDefault={() => navigateTo("/")}>
+            <div class="text-sm md:text-lg">TOP</div>
+          </a>
+        </div>
+      </AppBar>
+    </div>
 
-  <div class="container mx-auto overflow-y-auto pb-16">
-    <slot />
+    <button class="fixed bottom-4 right-4 bg-gray-800 text-white p-2 rounded" on:click={toggleTheme}>
+      Toggle Theme
+    </button>
+
+    <div class="container mx-auto overflow-y-auto pb-16">
+      <slot />
+    </div>
   </div>
-</div>
+{:else}
+  <div class="flex items-center justify-center h-screen bg-gray-100">
+    <div class="font-mono text-black text-2xl">Now Loading...</div>
+  </div>
+{/if}
--- skeleton-app/tailwind.config.ts
+++ skeleton-app/tailwind.config.ts
@@ -4,6 +4,19 @@ import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
 import { skeleton } from "@skeletonlabs/tw-plugin";
 
+enum Theme {
+  Skeleton = "skeleton",
+  Wintry = "wintry",
+  Modern = "modern",
+  Rocket = "rocket",
+  Seafoam = "seafoam",
+  Vintage = "vintage",
+  Sahara = "sahara",
+  Hamlindigo = "hamlindigo",
+  GoldNouveau = "gold-nouveau",
+  Crimson = "crimson",
+}
+
 export default {
   darkMode: "class",
   content: [
@@ -18,12 +31,10 @@ export default {
     typography,
     skeleton({
       themes: {
-        preset: [
-          {
-            name: "skeleton",
-            enhancements: true,
-          },
-        ],
+        preset: Object.values(Theme).map((theme) => ({
+          name: theme,
+          enhancements: true,
+        })),
       },
     }),
   ],
