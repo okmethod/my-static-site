470d5d47394d8eb572cfcd426345822d496a713f
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-08 22:25:00 +0900

    :+1: use theme skeleton light

--- skeleton-app/tailwind.config.ts
+++ skeleton-app/tailwind.config.ts
@@ -20,7 +20,7 @@ export default {
       themes: {
         preset: [
           {
-            name: "rocket",
+            name: "skeleton",
             enhancements: true,
           },
         ],
