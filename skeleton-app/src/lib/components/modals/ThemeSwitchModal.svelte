0f054a5ee9ef5c788f9884b2b0cbaa7b4e65b5bf
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-09 11:04:26 +0900

    :+1: responsive ThemeSwitchModal

--- skeleton-app/src/lib/components/modals/ThemeSwitchModal.svelte
+++ skeleton-app/src/lib/components/modals/ThemeSwitchModal.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { getModalStore } from "@skeletonlabs/skeleton";
   import { SlideToggle } from "@skeletonlabs/skeleton";
-  import Icon from "@iconify/svelte";
   import type { ThemeName } from "$lib/stores/theme";
   import { themeNames, getTheme, setTheme } from "$lib/stores/theme";
 
@@ -27,14 +26,14 @@
 </script>
 
 {#if $modalStore[0]}
-  <div class="p-4 bg-surface-50-900-token rounded" data-parent={parent}>
+  <div class="p-2 md:p-4 bg-surface-50-900-token rounded" data-parent={parent}>
     <div class="relative pt-8">
       <div class="flex flex-col items-center space-y-2">
-        <div class="grid grid-cols-4 gap-2">
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-2">
           {#each themeNames as themeName}
             <button
               type="button"
-              class="btn variant-filled"
+              class="btn {currentTheme === themeName ? 'variant-filled-surface' : 'variant-filled'} min-w-40"
               on:click={() => {
                 handleThemeSwitch(themeName);
               }}>{themeName}</button
@@ -45,8 +44,12 @@
           >{currentDark ? "Dark Mode" : "Light Mode"}</SlideToggle
         >
       </div>
-      <button on:click={closeModal} class="absolute top-0 right-6 z-10">
-        <Icon icon="mdi:close" class="w-5 h-5" />
+      <button
+        type="button"
+        class="btn-icon btn-icon-sm !bg-transparent absolute top-0 right-0 z-10"
+        on:click={closeModal}
+      >
+        ✕
       </button>
     </div>
   </div>
